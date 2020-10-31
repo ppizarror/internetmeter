ffcf226c4fd877471eb459ed32ca1954245cf052
ppizarror <pablo.pizarro@ing.uchile.cl>
2020-10-31 17:16:40 -0300

    Added error logging

--- internetmeter/_datastore.py
+++ internetmeter/_datastore.py
@@ -30,25 +30,40 @@ from pyfastcom import PyFastCom
 import os
 
 
+def check_create_file(filename: str) -> None:
+    """
+    Check file, if not exists then create.
+
+    :param filename: Name of the file
+    """
+    if not os.path.isfile(filename):
+        f = open(filename, 'w')
+        f.close()
+    assert os.path.isfile(filename), 'file {0} does not exist'.format(filename)
+
+
 class DataStore(object):
     """
     Store the data from a query.
     """
-    _datafile: str
+    _data_file: str
+    _dataview_file: str
+    _error_file: str
     _fast: 'PyFastCom'
 
-    def __init__(self, datafile: str, fast: 'PyFastCom'):
+    def __init__(self, data_file: str, dataview_file: str, error_file: str, fast: 'PyFastCom'):
         """
         Constructor.
 
-        :param datafile: Data file
+        :param data_file: Data file
         :param fast: Fast.com object
         """
-        if not os.path.isfile(datafile):
-            f = open(datafile, 'w')
-            f.close()
-        assert os.path.isfile(datafile), 'Data file {0} does not exist'.format(datafile)
-        self._datafile = datafile
+        check_create_file(data_file)
+        check_create_file(dataview_file)
+        check_create_file(error_file)
+        self._data_file = data_file
+        self._dataview_file = dataview_file
+        self._error_file = error_file
         self._fast = fast
 
     def store(self) -> None:
@@ -56,7 +71,7 @@ class DataStore(object):
         Store the query results to file.
         """
         assert self._fast.ready(), 'Fast results not ready'
-        with open(self._datafile, 'a') as myfile:
+        with open(self._data_file, 'a') as myfile:
             myfile.write('{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\n'.format(
                 datetime.today().strftime('%Y-%m-%d\t%H:%M:%S'),
                 self._fast.get_download_speed()[0],
@@ -68,3 +83,12 @@ class DataStore(object):
                 self._fast.get_upload_latency()[0],
                 self._fast.get_upload_latency()[1]
             ))
+
+    def log_error(self, error_name: str) -> None:
+        """
+        Write error info to file.
+
+        :param error_name: Name of the error
+        """
+        with open(self._error_file, 'a') as myfile:
+            myfile.write('{0}\t{1}\n'.format(datetime.today().strftime('%Y-%m-%d %H:%M:%S'), error_name))
